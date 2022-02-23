a2f5be8fee1a37ce22efeabbdec31796e2cad699
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-02-23 10:24:56 +0000

    Add code from stock data challenge

--- run.py
+++ run.py
@@ -148,7 +148,18 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
 
+
+def get_stock_values(data):
+    headings = stock = SHEET.worksheet('stock').row_values(1)
+
+    stock_dict = {heading:value for heading, value in zip(headings, data)}
+    return(stock_dict)
+    
+    
+stock_values = get_stock_values(stock_data)
+print(stock_values)
